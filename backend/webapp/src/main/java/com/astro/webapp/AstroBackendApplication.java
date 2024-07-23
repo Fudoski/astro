bf4f68229be6ebfd23b9130158f4929832bc1def
Oleksii Bibik <lafan4ik007@gmail.com>
2024-07-23 21:34:29 +0300

    Initialized backend with spring boot MVC

---
+++ backend/webapp/src/main/java/com/astro/webapp/AstroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.astro.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroBackendApplication.class, args);
+	}
+
+}
---
+++ backend/webapp/src/test/java/com/astro/webapp/AstroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
