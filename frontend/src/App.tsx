ddcfafb2e7e05e590ef83679081d8dfb319b23ac
Oleksii Bibik <lafan4ik007@gmail.com>
2024-07-24 22:26:58 +0300

    Added ssl and cors configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import "./App.css";
+
+axios.defaults.baseURL = "https://localhost:8443";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const checkBackend = async () => {
+    axios.get("/hello").then((res) => console.log(res.data));
+  };
 
   return (
-    <>
-      <div className='bg-black'>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-yellow-100'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <button type="button" onClick={checkBackend}>
+        check backend
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
